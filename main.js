48cd35da7663a36e612866865da2356efa17fd5f
Marshall Danel <marshalldanel@gmail.com>
2017-09-30 13:36:22 -0700

    Update/formatting

--- main.js
+++ main.js
@@ -2,94 +2,94 @@
 // Select all links with hashes
 $('a[href*="#"]')
 // Remove links that don't actually link to anything
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function(event) {
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
   // On-page links
-  if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
-    location.hostname == this.hostname
-  ) {
+    if (
+      location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+    &&
+    location.hostname === this.hostname
+    ) {
     // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    // Does a scroll target exist?
-    if (target.length) {
+      let target = $(this.hash);
+      target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+      // Does a scroll target exist?
+      if (target.length) {
       // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top,
+        }, 1000, () => {
         // Callback after animation - Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
+          const $target = $(target);
+          $target.focus();
+          if ($target.is(':focus')) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        });
+      }
     }
-  }
-});
+  });
 
 // Chatty modal logic
-const chattyModal = document.getElementById('chattyModal')
+const chattyModal = document.getElementById('chattyModal');
 const chattyModalBtn = document.getElementById('chattyModalBtn');
 const chattyCloseBtn = document.getElementsByClassName('closeBtn')[1];
 
 chattyModalBtn.onclick = () => {
   chattyModal.style.display = 'block';
-}
+};
 
-chattyCloseBtn.onclick =() => {
+chattyCloseBtn.onclick = () => {
   chattyModal.style.display = 'none';
-}
+};
 
 // RoadieSounds modal logic
-const roadieModal = document.getElementById('roadieModal')
+const roadieModal = document.getElementById('roadieModal');
 const roadieModalBtn = document.getElementById('roadieModalBtn');
 const roadieCloseBtn = document.getElementsByClassName('closeBtn')[0];
 
 roadieModalBtn.onclick = () => {
   roadieModal.style.display = 'block';
-}
+};
 
-roadieCloseBtn.onclick =() => {
+roadieCloseBtn.onclick = () => {
   roadieModal.style.display = 'none';
-}
+};
 
 // Schooodle modal logic
-const schooodleModal = document.getElementById('schooodleModal')
+const schooodleModal = document.getElementById('schooodleModal');
 const schooodleModalBtn = document.getElementById('schooodleModalBtn');
 const schooodleCloseBtn = document.getElementsByClassName('closeBtn')[2];
 
 schooodleModalBtn.onclick = () => {
   schooodleModal.style.display = 'block';
-}
+};
 
-schooodleCloseBtn.onclick =() => {
+schooodleCloseBtn.onclick = () => {
   schooodleModal.style.display = 'none';
-}
+};
 
 // Jungle modal logic
-const jungleModal = document.getElementById('jungleModal')
+const jungleModal = document.getElementById('jungleModal');
 const jungleModalBtn = document.getElementById('jungleModalBtn');
 const jungleCloseBtn = document.getElementsByClassName('closeBtn')[3];
 
 jungleModalBtn.onclick = () => {
   jungleModal.style.display = 'block';
-}
+};
 
-jungleCloseBtn.onclick =() => {
+jungleCloseBtn.onclick = () => {
   jungleModal.style.display = 'none';
-}
+};
 
 // Universal modal close on outside click
 document.onclick = (e) => {
-  if (e.target == chattyModal || e.target == roadieModal || e.target == schooodleModal || e.target == jungleModal) {
+  if (e.target === chattyModal || e.target === roadieModal || e.target === schooodleModal || e.target === jungleModal) {
     e.target.style.display = 'none';
   }
-}
+};
